3512404f43520118a16ad3a6c5ee752243dfe67b
CoreByte <21132799+gCoreByte@users.noreply.github.com>
2025-05-07 01:17:24 +0300

    Fix account name

--- lolpros_api.py
+++ lolpros_api.py
@@ -21,14 +21,14 @@ class LolprosApi:
         if len(keys) == 0 or isinstance(value, str):
             return value
         if value is None:
-            return None
-        value = value.get(keys[0], None)
+            return ""
+        value = value.get(keys[0], "")
         return self._dig(value, keys[1:])
 
     def _get_player_name(self, participant: {}, account: Account):
         if participant['riotId'].lower().strip() == account.full_name():
             return "CGN Reptile"
-        return self._dig(participant['lolpros'], "name")
+        return f"{self._dig(participant['lolpros'], 'team', 'tag')} {self._dig(participant['lolpros'], 'name')}".strip()
 
     async def get_all_pro_names(self, account: Account):
         data = await self._get_lolpros_data(account)
@@ -43,13 +43,6 @@ class LolprosApi:
         for participant in data['participants']:
             champion_name = champion_data[participant['championId']]['name']
             player_name = self._get_player_name(participant, account)
-            if player_name is not None:
-                if participant['lolpros'] is not None:
-                    team = participant['lolpros'].get('team')
-                    if team is not None:
-                        team_tag = team.get('tag', "")
-                        player_name = f"{team_tag} {player_name}".strip()
-
             formatted_string = f"{champion_name} ({player_name})"
             if participant['teamId'] == 100:
                 if player_name is not None:
